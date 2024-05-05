07a0ffe6001469b55b895ea840b98b69890f1711
B Karthik <139115864+Karth-i@users.noreply.github.com>
2024-05-05 22:27:51 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -68,7 +68,7 @@ def main():
             # Load model
             model_url = "https://github.com/Karth-i/New_One/raw/9ba3e1c71a83bf70df186c342b837a9745721849/model1.h5"
             response = requests.get(model_url)
-            with open("model.h5", "wb") as f:
+            with open("model1.h5", "wb") as f:
                 f.write(response.content)
             model = tf.keras.models.load_model("model.h5")
 
