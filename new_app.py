ca8e42e44db73332444776fc67b9670d1f4f2d1a
B Karthik <139115864+Karth-i@users.noreply.github.com>
2024-05-05 22:36:05 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -4,7 +4,7 @@ import numpy as np
 import re
 from collections import defaultdict
 import requests
-from keras.initializers import Orthogonal
+from keras.initializers import Orthogonal1
 
 # Function to extract English words from a text
 def extract_english_words(text):
