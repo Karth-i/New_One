d91c55fdee85fdcfb8376ad8e2e4c001bdc756b4
B Karthik <139115864+Karth-i@users.noreply.github.com>
2024-05-07 07:49:55 +0530

    Update np_app.py

--- np_app.py
+++ np_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 import numpy as np
+import request
 
 # Load the saved model
 url = 'https://github.com/Karth-i/New_One/raw/main/new_np1.keras'
